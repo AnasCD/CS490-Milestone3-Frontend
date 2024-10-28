ef36fc696cf4f81362f641dfd76f17f2aedf6895
Anas Sabir <37444844+AnasCD@users.noreply.github.com>
2024-10-27 22:55:48 -0400

    Delete backend directory

--- backend/routes/films.js
+++
@@ -1,7 +0,0 @@
-import express from "express";
-import { getFilmDetails } from "../controllers/filmsController.js";
-
-const router = express.Router();
-router.get("/:id", getFilmDetails);
-
-export default router;
--- backend/routes/rentals.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { rentFilm } from '../controllers/rentalsController.js';
-
-const router = express.Router();
-
-
-router.post('/', rentFilm);
-
-export default router;
